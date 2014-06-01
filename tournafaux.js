72b34598474f82f6ff821e154f47a90e0c10c683
Bennie Lundmark <bennie.lundmark@gmail.com>
2014-06-01 22:57:02 +0200

    Round 1 page shows

--- tournafaux.js
+++ tournafaux.js
@@ -1,25 +1,253 @@
+$(function() {
+
+	var Player = Backbone.Model.extend({
+
+		initialize: function() {
+
+      		if (!this.get("name")) {
+        		this.set({"name": "No name?"});
+      		}
+    	},
+
+    	getPreviousOpponents: function() {
+    		var i = 1;
+    		var opponents = [];
+    		while (this.get("opponent"+i)) {
+    			opponents.push(this.get("opponent"+i))
+    			i++;
+    		}
+    		return opponents;
+    	},
+
+    	countPointsWithBye: function(pointType, byeScore) {
+    		var i = 1;
+			var total = 0;
+    		var bye = false;
+    		var realGames = 0;
+    		while(this.get(pointType+i)) {
+    			if (this.get(pointType+i) === "-") {
+    				bye = true;
+    			} else {
+    				total += parseInt(this.get(pointType+i));
+    				realGames += 1;
+    			}
+    			i++;
+    		}
+    		if (bye) {
+    			if (realGames == 0)
+    				total = byeScore;
+    			else
+    				total += (total / realGames);
+    		}
+    		return total;
+    	},
+
+    	getTotalTp: function() {
+    		return this.countPointsWithBye("tp", 3);
+    	},
+
+    	getTotalVp: function() {
+    		return this.countPointsWithBye("vp", 5);
+    	},
+
+    	getVpDiff: function() {
+    		return this.countPointsWithBye("vpdiff", 1);
+    	},
+
+	});
+
+	var PlayerList = Backbone.Collection.extend({
+
+		model: Player,
 	
+		localStorage: new Backbone.LocalStorage("tournafaux-players"),
+
+	});
+
+	var Players = new PlayerList;
+
+
+	var Round = Backbone.Model.extend({
+		
+		initialize: function() {
+      		if (!this.get("number")) {
+        		this.set({"number": "666"});
+      		}
+    	},
+	});
+
+	var RoundList = Backbone.Collection.extend({
+
+		model: Round,
+
+		localStorage: new Backbone.LocalStorage("tournafaux-rounds"),
+
+		newRound: function(number, maxNumber) {
+			console.log(this.models);
+
+			var round = _.find(this.models, function(round){ return round.get("number") == ""+number});
+			console.log(round);
+			if (!round) {
+				round = this.create({number: ""+number, maxNumber: maxNumber});
+				console.log(this.models);
+			} else {
+				_.each(Players.models, function(p) {p.unset("opponent"+number)});
+			}
+
+			// Create bye if needed
+			if (Players.models.length % 2 == 1)
+				Players.create({id:"0", name:"-"})
+			var players = Players.models;
+
+			players = _.shuffle(players);
+
+			console.log(_.map(players, function(player) {return player.get('name')}));
+
+			players = _.sortBy(players, function(p) {return p.getTotalVp()});
+			players = _.sortBy(players, function(p) {return p.getVpDiff()});
+			players = _.sortBy(players, function(p) {return p.getTotalTp()});
+
+			console.log(_.map(players, function(player) {return player.get('name')}));
+
+			var table = 1;
+			while(players.length > 0) {
+				var p1 = players.pop();
+				var p2 = players.pop();
+				var prevOpps = [];
+
+				while(_.indexOf(p1.getPreviousOpponents(), p2.id) >= 0) {
+					prevOpps.push(p2);
+					p2 = players.pop();
+				}
+				while(prevOpps.length > 0)
+					players.push(prevOpps.pop());
+
+				p1.set("opponent" + number, p2.id);
+				p1.save();
+				p2.set("opponent" + number, p1.id);
+				p2.save();
+				round.set("table"+table+"player1", p1.id);
+				round.set("table"+table+"player2", p2.id);
+				round.save();
+				table ++;
+			}
+
+		},
+	});
+
+	var Rounds = new RoundList();
+
 	var TournamentSettingsView = Backbone.View.extend({
+		
 		el: '.page',
+
+		events: {
+			"keypress #new-player": "createOnEnter",
+			"click button.removePlayer": "removePlayer",
+			"click #generate-round": "generateRound"
+		},
+		
+		initialize: function() {
+			this.listenTo(Players, 'add', this.render);
+			this.listenTo(Players, 'remove', this.render);
+			this.listenTo(Players, 'reset', this.render);
+			// this.listenTo(router, 'route:settings', this.render);
+		
+			Players.fetch();
+			Rounds.fetch();
+
+		},
+		
 		render: function(options) {
-			var template = _.template($('#tournament-settings-template').html(), {});
-          	this.$el.html(template);
+			var template = _.template($('#tournament-settings-template').html(), {players: Players.models});
+	      	this.$el.html(template);
+	      	this.newPlayer = this.$("#new-player");
+		},
+
+		createOnEnter: function(e) {
+			if (e.keyCode != 13) return;
+			if (!this.newPlayer.val()) return;
+			Players.create({name: this.newPlayer.val()});
+			this.newPlayer.val('');
+			this.newPlayer.focus();
+		},
+
+		removePlayer: function(e) {
+			Players.get(e.currentTarget.id).destroy();
+			// Players.remove().destroy();
+			return false;
+		},
+
+		generateRound: function() {
+			//TODO validation
+			Rounds.newRound(1, this.$("#rounds").val());
+			router.navigate("#/round/1");
+			return false;
 		}
 	});
-	
+
 	var tournamentSettingsView = new TournamentSettingsView();
 
+	var TournamentRoundView = Backbone.View.extend({
+
+		el: '.page',
+
+		initialize: function() {
+			Players.fetch();
+
+			Rounds.fetch();
+		},
+
+		render: function(number) {
+			this.round = _.find(Rounds.models, function(round){ return round.get("number") == "" + number});
+			console.log(this.round);
+			
+			var tables = [];
+			var i = 1;
+
+			while (this.round.get('table'+i+'player1')) {
+				var player1 = Players.get(this.round.get('table'+i+'player1'));
+				var player2 = Players.get(this.round.get('table'+i+'player2'));
+				tables.push({number: ""+i,
+					player1name: player1.get('name'),
+					player1vp: player1.get('vp') ? player1.get('vp') : "",
+					player1id: player1.id,
+					player2name: player2.get('name'),
+					player2vp: player2.get('vp') ? player2.get('vp') : "",
+					player2id: player2.id});
+				++i;
+			};
+
+			console.log(tables);
+
+			if (this.round) {
+				var template = _.template($('#tournament-round-template').html(), {number: this.round.get('number'), tables: tables});
+		      	this.$el.html(template);
+			} else {
+				//TODO display error
+			}
+		}
+
+	});
+
+	var tournamentRoundView = new TournamentRoundView();
+
 	var Router = Backbone.Router.extend({
-        routes: {
-          "": "home"
-          
-        }
-    });
+	    routes: {
+	      "": "settings",
+	      "round/:number": "round",
+	    }
+	});
 
 	var router = new Router;
-    router.on('route:home', function() {
-      tournamentSettingsView.render({});
-    });
-    
+	router.on('route:settings', function() {
+	  tournamentSettingsView.render({});
+	});
+	router.on('route:round', function(number) {
+		console.log("round route " + number);
+		tournamentRoundView.render(number);
+	});
+
     Backbone.history.start();
   
+});
